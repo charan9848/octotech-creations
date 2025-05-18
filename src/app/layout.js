a7dda5fb12408c0963f9be6733e826c7d87d42b1
Gannoj Bhanu Charan <gannojcharan@gmail.com>
2025-05-18 11:09:31 +0530

    speed anylatics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
 "use client";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from '@mui/material/styles';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import theme from './theme';
 import Navbar from '@/components/navbar/navbar';
 import Footer from '@/components/footer/footer';
@@ -42,6 +43,7 @@ export default function RootLayout({ children }) {
           <main>
             {children}
             <Analytics />
+            <SpeedInsights />
           </main>
           <footer>
             <Footer />
