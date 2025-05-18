6970138f6a5078517d83e84f101fbf912cf0a655
Gannoj Bhanu Charan <gannojcharan@gmail.com>
2025-05-18 17:28:55 +0530

    backend

--- src/app/api/register-user/route.js
+++ src/app/api/register-user/route.js
@@ -1,6 +1,8 @@
 import clientPromise from "@/lib/mongodb";
 import bcrypt from "bcryptjs";
 
+const MAX_ARTISTS = 4; // Set your limit
+
 export async function POST(request) {
   try {
     const data = await request.json();
@@ -8,9 +10,20 @@ export async function POST(request) {
     const hashedPassword = await bcrypt.hash(data.password, 10);
     data.password = hashedPassword;
 
+    // Connect to your database
     const client = await clientPromise;
     const db = client.db(process.env.MONGODB_DB);
     const users = db.collection("artists");
+
+    // Count current artists
+    const artistCount = await db.collection("artists").countDocuments();
+    if (artistCount >= MAX_ARTISTS) {
+      return Response.json(
+        { error: "Artist registration limit reached." },
+        { status: 400 }
+      );
+    }
+
     await users.insertOne(data);
     return Response.json({ message: "User registered!" });
   } catch (err) {
